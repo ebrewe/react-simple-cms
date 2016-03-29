a724e3dca7049dc07a629c6e67b9752a1ecbc539
ebrewe <ericdbrewer@gmail.com>
2016-03-29 19:52:41 -0400

    index shows recent posts

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -10,6 +10,17 @@ class PostsIndex extends Component{
      this.props.fetchPosts();
   }
 
+  renderPosts(){
+     return this.props.posts.map( (post)=> {
+       return (
+          <li className="list-group-item" key={post.id}>
+            <span className="pull-xs-right">{post.categories}</span>
+            <strong>{post.title}</strong>
+          </li>
+       );
+     })
+  }
+
   render(){
      return(
         <div>
@@ -18,9 +29,18 @@ class PostsIndex extends Component{
                Add a Post
              </Link>
           </div>
+          <h3>Recent Posts</h3>
+          <ul className="list-group">
+            {this.renderPosts()}
+          </ul>
         </div>
      );
   }
+}
+
+
+function mapStateToProps(state){
+  return { posts: state.posts.all};
 }
 
 /*function mapDispatchToProps(dispatch){
@@ -31,4 +51,4 @@ export default connect(null, mapDispatchToProps)(PostsIndex);*/
 //instead of above, do:
 //export default connect(null, {fetchPosts: fetchPosts})(PostsIndex);
 //or: shortened further,
-export default connect(null, {fetchPosts})(PostsIndex);
+export default connect(mapStateToProps, {fetchPosts})(PostsIndex);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -5,6 +5,7 @@ const INITIAL_STATE = {all: [], post: null};
 export default function(state = INITIAL_STATE, action){
    switch(action.type){
      case FETCH_POSTS:
+        console.log(state, action.payload.data)
         return {...state, all: action.payload.data};
 
      default:
