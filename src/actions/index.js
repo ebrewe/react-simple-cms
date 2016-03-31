c47ec6a41bdaa35bf6520a6ebbe0ab0217f46bea
ebrewe <ericdbrewer@gmail.com>
2016-03-31 11:08:38 -0400

    delete post, redirect to index

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
 export const FETCH_POST = "FETCH_POST";
+export const DELETE_POST = "DELETE_POST";
+export const ACTIONS = {FETCH_POST, CREATE_POST, FETCH_POSTS, DELETE_POST};
 
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
 const API_KEY = '?key=ebrewe-simple-cms';
@@ -35,4 +37,12 @@ export function fetchPost(id){
      type: FETCH_POST,
      payload: request
    }
+}
+
+export function deletePost(id){
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+  return{
+    type: ACTIONS.DELETE_POST,
+    payload:request
+  }
 }
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,11 @@
-import { FETCH_POSTS, FETCH_POST } from '../actions/index';
+import { FETCH_POSTS, FETCH_POST, ACTIONS } from '../actions/index';
 
 const INITIAL_STATE = {all: [], post: null};
 
 export default function(state = INITIAL_STATE, action){
+
+   console.log(ACTIONS.FETCH_POSTS == action.type)
+
    switch(action.type){
      case FETCH_POSTS:
         return {...state, all: action.payload.data};
